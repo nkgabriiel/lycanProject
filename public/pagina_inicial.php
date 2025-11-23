f6e5ef3cac4882d5b8091af7e42d7eb139df4a36
lucas_cimino <lucasciminoalves@gmail.com>
2025-11-23 00:23:15 -0300

    mensagem que voce quer

--- public/pagina_inicial.php
+++ public/pagina_inicial.php
@@ -15,13 +15,39 @@ $usuario_exib = htmlspecialchars($_SESSION['usuario_nome'] ?? 'Usuario', ENT_QUO
     <link rel="stylesheet" href="../assets/style.css">
 </head>
 <body>
-    <div class="tela-inicialcliente">
-    <h1>Página Inicial</h1>
-    <h2>Bem-vindo(a), <?= $usuario_exib ?>!</h2>
-    
-    <p>Você está logado como um usuário comum.</p>
-    
-    <p><a href="../app/logout.php", class="btn-saircliente">Sair</a></p>
+    <header class="header">
+        <section>
+        <a href="pagina_inicial.php" class="logo">
+            <img src="../assets/img/iconlycan.png" alt="logo">
+        </a>
+
+    <nav class="navbar">
+        <a href="pagina_inicial.php">HOME</a>
+        <a href="#male">MASCULINO</a>
+        <a href="#female">FEMININO</a>
+        <a href="#about">CONTATO</a>
+    </nav>
+
+    <div class="icons">
+        <a  href="#">
+        <img width="35" height="35" class="search" src="https://img.icons8.com/ios-filled/50/search--v1.png" alt="search--v1"/>
+    </a>
+    <a  href="#">
+        <img width="35" height="35" class="cart" src="https://img.icons8.com/ios-glyphs/30/shopping-cart--v1.png" alt="shopping-cart--v1"/>
+    </a>
+    <a  href="#"> 
+        <img width="35" height="35" src="https://img.icons8.com/ios-glyphs/30/user-male-circle.png" alt="user-male-circle"/>
+    </a>   
+    </div>
+     </section>
+    </header>
+        <div class="home-container">
+        <section id="home">
+            <div class="content">
+                <h3>LANÇAMENTOS E PROMOÇÕES</h3>
+                <a href="#" class="btn-homepage">Garanta já</a>
+            </div>
+        </section>
     </div>
 </body>
 </html>
