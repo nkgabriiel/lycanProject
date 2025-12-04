f2f6877b3b1658e490bfb4bc38d5a7ef4a8f4707
nkgabriiel <gabriel.3930@gmail.com>
2025-12-04 17:12:13 -0300

    teste 2

---
+++ public/test_env.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "<pre>";
+print_r([
+    'DB_HOST' => getenv('DB_HOST'),
+    'DB_PORT' => getenv('DB_PORT'),
+    'DB_NAME' => getenv('DB_NAME'),
+    'DB_USER' => getenv('DB_USER'),
+    'DB_PASS' => getenv('DB_PASS'),
+]);
